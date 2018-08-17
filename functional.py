3ef0c9bbf2f4596a757a84093b6bdc6cf6d1b3fb
Apollo <sushi90817564@gmail.com>
2018-08-17 15:15:13 +0800

    fix thres_iou performance issue

---
+++ functional.py
@@ -0,0 +1,59 @@
+from lightai.imps import *
+
+def create_val(ds):
+    trn_idx, valid_idx = split_idx(len(ds.img), 0.8, seed=1)
+    trn_path = Path('inputs/train')
+    val_path = Path('inputs/val')
+    val_path.mkdir()
+    (val_path/'images').mkdir()
+    (val_path/'masks').mkdir()
+    for idx in valid_idx:
+        fname = ds.img[idx].parts[-1]
+        (trn_path/'images'/f'{fname}').rename(val_path/'images'/f'{fname}')
+        (trn_path/'masks'/f'{fname}').rename(val_path/'masks'/f'{fname}')
+
+def rl_enc(img):
+    pixels = img.flatten('F')
+    a = np.concatenate([[0],pixels])
+    b = np.concatenate([pixels,[0]])
+    start = np.where(b - a == 1)[0] + 1
+    end = np.where(a - b == 1)[0] + 1
+    length = end - start
+    res = np.zeros(2*len(start),dtype=int)
+    res[::2] = start
+    res[1::2] = length
+    return ' '.join(str(x) for x in res)
+
+def iou(predict, target, threshold):
+    """return a batch of images' score at threshold t
+    """
+    predict, target = predict.view(-1,101*101), target.view(-1,101*101)
+    metric = torch.arange(0.5,1,0.05, device='cuda')
+    intersection = torch.sum((predict>threshold)*(target==1),dim=1)
+    union = torch.sum(predict>threshold,dim=1)+torch.sum(target==1,dim=1)-intersection
+    percentage = intersection.float()/union.float()
+    percentage[torch.isnan(percentage)] = 1
+    percentage = percentage.view(-1,1)
+    percentage = percentage.expand(-1,10)
+    return torch.sum(percentage>metric,dim=1).float()/10
+
+def thres_iou(model, val_dl):
+    thresholds = np.linspace(0,1,num=100,endpoint=False)
+    res = []
+    with torch.no_grad():
+        model.eval()
+        for img, mask in val_dl:
+            batch_res = []
+            img,mask = T(img),T(mask)
+            predict = torch.sigmoid(model(img))
+            for t in thresholds:
+                batch_res.append(iou(predict, mask, t))
+            res.append(torch.stack(batch_res))
+    return thresholds,np.array(torch.cat(res,dim=1).mean(dim=1))
+
+def visualize(ds, id):
+    _, ax = plt.subplots(1,2)
+    img, mask = ds[id]
+    print(ds.img[id])
+    ax[0].imshow(img[0])
+    ax[1].imshow(mask[0])
---
+++ model.py
@@ -0,0 +1,58 @@
+from lightai.imps import *
+
+class UnetBlock(nn.Module):
+    def __init__(self,feature_c, x_c):
+        """input channel size: feature_c, x_c
+        output channel size: feature_c
+        """
+        super().__init__()
+        self.upconv = nn.ConvTranspose2d(x_c, feature_c, kernel_size=3, stride=2, padding=1)
+        self.conv1 = nn.Conv2d(feature_c*2, feature_c, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(feature_c, feature_c, kernel_size=3, stride=1, padding=1)
+        self.bn = nn.BatchNorm2d(feature_c)
+        
+    def forward(self, feature, x):
+        x = self.upconv(x,output_size=feature.shape)
+        x = torch.cat([x, feature], dim=1)
+        x = torch.relu(self.conv1(x))
+        x = torch.relu(self.conv2(x))
+        return self.bn(x)
+
+class Dynamic(nn.Module):
+    def __init__(self, encoder, ds):
+        super().__init__()
+        self.encoder = encoder
+        self.features = []
+        self.handles = []
+        hook_fn = lambda module,input:self.features.append(input[0])
+        for m in encoder.children():
+            handle = m.register_forward_pre_hook(hook_fn)
+            self.handles.append(handle)
+        self.dummy_forward(T(np.transpose(ds[[0]][0],axes=[0,3,1,2]), cuda=False))
+
+    def forward(self,x):
+        x = self.encoder(x)
+        for feature,block in zip(reversed(self.features),self.upmodel):
+            x = block(feature,x)
+        self.features = []
+        x = self.final_conv(x)
+        return x
+        
+    def get_layer_groups(self):
+        return [self.encoder,[self.upmodel,self.final_conv]]
+
+    def dummy_forward(self,x):
+        x = self.encoder(x)
+        upmodel = []
+        for i in reversed(range(len(self.features))):
+            feature = self.features[i]
+            if feature.shape[2] != x.shape[2]:
+                block = UnetBlock(feature.shape[1], x.shape[1])
+                x = block(feature,x)
+                upmodel.append(block)
+            else:
+                self.handles[i].remove()
+        self.features = []
+        self.upmodel = nn.ModuleList(upmodel)
+        self.final_conv = nn.Conv2d(x.shape[1],1,kernel_size=1)
+    
