4f9dc2ff33a5ff2bfff5a6479fcacb1b8a191fa0
Apollo <sushi90817564@gmail.com>
2018-08-29 08:27:33 +0800

    save

--- script.py
+++ script.py
@@ -7,8 +7,9 @@ from transform import *
 from dataset import *
 from metric import *
 import torchvision.transforms as transforms
+from wide_res import *
 
-bs = 32
+bs = 16
 wd = 1e-6
 lr = 0.3
 
@@ -18,39 +19,51 @@ def get_data(trn_tsfm):
     sp_trn_ds = ImageDataset('inputs/gray/sample/train',tsfm=trn_tsfm)
     sp_val_ds = ImageDataset('inputs/gray/sample/val',tsfm=val_tsfm)
     sp_trn_sampler = BatchSampler(sp_trn_ds, bs)
-    sp_val_sampler = BatchSampler(sp_val_ds, bs)
+    sp_val_sampler = BatchSampler(sp_val_ds, int(bs*1.5))
     sp_trn_dl = DataLoader(sp_trn_sampler)
     sp_val_dl = DataLoader(sp_val_sampler)
     return sp_trn_ds,sp_trn_dl,sp_val_dl
 
-def reset_learner(trn_tsfm):
+def reset_learner(trn_tsfm,width,drop):
     sp_trn_ds,sp_trn_dl,sp_val_dl = get_data(trn_tsfm)
-    resnet = resnet18(False)
-    model = Dynamic(model_cut(resnet,-2), sp_trn_ds).cuda()
+    # resnet = resnet18(False, width=width,drop=drop)
+    resnet = Wide_ResNet(16, width, 0.3)
+    model = Dynamic(resnet, sp_trn_ds, drop=drop).cuda()
     layer_opt = LayerOptimizer(model)
     learner = Learner(sp_trn_dl, sp_val_dl, model, nn.BCEWithLogitsLoss(),layer_opt,
                     metric=Score, small_better=False, sv_best_path='./model/best')
     return learner
 
-def test(tsfm, epoch):
-    print(f'epoch={epoch}, tsfm=\n{tsfm}')
-    learner = reset_learner(tsfm)
-    learner.fit(n_epochs=epoch, lrs=lr,wds=wd,clr_params=[50,5,0.01,0], print_stats=False)
+def test(tsfm, epoch, width, drop):
+    print((
+        f'epoch={epoch}\n'
+        f'width={width}\n'
+        f'drop={drop}\n'
+        f'bs={bs}\n'
+        f'tsfm={tsfm}'
+    ))
+    learner = reset_learner(tsfm,width,drop)
+    learner.fit(n_epochs=epoch, lrs=lr,wds=wd,clr_params=[50,5,0.01,0.1], print_stats=False)
     print('----------------------------')
 
 distort = Distort(5,5,5)
 intensity = MyColorJitter(brightness=0.05)
 crop = CropRandom(0.7)
 param = {'degrees':0,'resample':Image.BICUBIC}
-zoom = MyRandomAffine(scale=[0.5,1.5],**param)
+zoom = MyRandomAffine(scale=[0.5,2],**param)
 zoom_in = MyRandomAffine(scale=[1,2],**param)
 zoom_out = MyRandomAffine(scale=[0.5,1],**param)
 shift = MyRandomAffine(translate=[0.5,0.5],**param)
 shear = MyRandomAffine(shear=45,**param)
 
-tsfm = MyRandomApply([sample_hflip,shift,intensity,MyRandomChoice(
+tsfm1 = MyRandomApply([sample_hflip,shift,intensity,MyRandomChoice(
     [distort,zoom,crop,shear],ps=[0.45,0.225,0.225,0.1])],ps=[0.5,0.5,0.5,0.5])
 
-for t in [None,distort,intensity,crop,zoom,zoom_in,zoom_out,shift,shear]:
-    for epoch in [8,16,32,64,128,256]:
-        test(t, epoch)
+tsfm2 = MyRandomApply([sample_hflip,shift,MyRandomChoice(
+    [distort,zoom,crop,shear],ps=[0.45,0.225,0.225,0.1])],ps=[0.5,0.5,0.5])
+
+tsfm3 = MyRandomApply([sample_hflip,shift,MyRandomChoice(
+    [Distort(5,5,10),zoom,crop,shear],ps=[0.45,0.225,0.225,0.1])],ps=[0.5,0.5,0.5])
+
+for epoch in [32]:
+    test(tsfm1,epoch,width=4,drop=0)
---
+++ unet.py
@@ -0,0 +1,61 @@
+class UnetBlock(nn.Module):
+    def __init__(self,feature_c, x_c, drop):
+        """input channel size: feature_c, x_c
+        output channel size: feature_c
+        """
+        super().__init__()
+        self.upconv1 = nn.ConvTranspose2d(x_c, feature_c, kernel_size=3, stride=2, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(feature_c*2, feature_c, kernel_size=3, stride=1, padding=1, bias=False)
+        self.upconv2 = nn.ConvTranspose2d(x_c, feature_c, kernel_size=3, stride=2, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(2*feature_c)
+        self.bn2 = nn.BatchNorm2d(feature_c)
+        self.bn3 = nn.BatchNorm2d(feature_c)
+        self.drop = nn.Dropout(drop)
+        
+    def forward(self, feature, x):
+        out = self.upconv1(x,output_size=feature.shape)
+        out = self.bn1(torch.relu(torch.cat([out, feature], dim=1)))
+        out = self.drop(out)
+        out = self.bn2(torch.relu(self.conv1(out))) + self.bn3(torch.relu(self.upconv2(x,output_size=feature.shape)))
+        return out
+
+class Dynamic(nn.Module):
+    def __init__(self, encoder, ds, drop):
+        super().__init__()
+        self.bn_input = nn.BatchNorm2d(1)
+        self.encoder = encoder
+        self.features = []
+        self.handles = []
+        hook_fn = lambda module,input:self.features.append(input[0])
+        for m in leaves(encoder):
+            handle = m.register_forward_pre_hook(hook_fn)
+            self.handles.append(handle)
+        self.dummy_forward(T(ds[0][0], cuda=False).unsqueeze(0), drop)
+
+    def forward(self,x):
+        x = self.bn_input(x)
+        x = self.encoder(x)
+        for feature,block in zip(reversed(self.features),self.upmodel):
+            x = block(feature,x)
+        self.features = []
+        x = self.final_conv(x)
+        return x
+        
+    def get_layer_groups(self):
+        return [[self.encoder],[self.upmodel,self.final_conv]]
+
+    def dummy_forward(self,x,drop):
+        with torch.no_grad():
+            x = self.encoder(x)
+        upmodel = []
+        for i in reversed(range(len(self.features))):
+            feature = self.features[i]
+            if feature.shape[2] != x.shape[2]:
+                block = UnetBlock(feature.shape[1], x.shape[1], drop)
+                x = block(feature,x)
+                upmodel.append(block)
+            else:
+                self.handles[i].remove()
+        self.features = []
+        self.upmodel = nn.ModuleList(upmodel)
+        self.final_conv = nn.Conv2d(x.shape[1],1,kernel_size=1)
