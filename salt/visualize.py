6eb80ca3f3f9901aebe8f8866c2efa534ee2eb4f
Apollo <sushi90817564@gmail.com>
2018-09-07 07:35:47 +0800

    save

--- salt/visualize.py
+++ salt/visualize.py
@@ -1,19 +1,18 @@
 from lightai.imps import *
+from salt.predict import *
 
 
-def visualize(model, size=3):
+def visualize(model, val_ds, size=3):
     """
     :param size: number of img to display
     """
-    tta_tsfms = [None, hflip]
     with torch.no_grad():
         model.eval()
         _, axes = plt.subplots(size, 4, figsize=(5 * size, 4 * size))
-        val_ds = CsvDataset('inputs/all/3/val.csv', tsfm=to_np, tta_tsfms=tta_tsfms)
         for i, ax in zip(np.random.randint(0, len(val_ds), size=size), axes):
             tta_batch = val_ds[i]
             predicts = []
-            for [img, mask], f in zip(tta_batch, tta_tsfms):
+            for [img, mask], f in zip(tta_batch, val_ds.tta_tsfms):
                 if not f:
                     raw_img = img
                 img = np.expand_dims(img, 0)
