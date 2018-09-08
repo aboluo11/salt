b8eacd11f49d92fbac95d7cd8cfb098185da1d3b
Apollo <sushi90817564@gmail.com>
2018-09-08 14:05:35 +0800

    save

--- distill_log.py
+++ distill_log.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 import pandas as pd
 import numpy as np
 
+
 def get_data():
     tsfms = defaultdict(dict)
     with open('log', 'r') as f:
@@ -18,17 +19,20 @@ def get_data():
         tsfms[tsfm][epoch] = score
     return tsfms
 
+
 def to_txt(tsfms):
-    with open('temp.txt','w') as f:
+    with open('temp.txt', 'w') as f:
         for tsfm, ep_scores in tsfms.items():
-            f.write(tsfm+':\n')
+            f.write(tsfm + ':\n')
             for ep, score in ep_scores.items():
                 f.write(f'  {ep}ep: {score}\n')
             f.write('\n')
 
+
 def to_csv(tsfms):
-    df = pd.DataFrame.from_dict(tsfms,orient='index')
+    df = pd.DataFrame.from_dict(tsfms, orient='index')
     df.to_csv('record.csv')
+
 
 tsfms = get_data()
 to_txt(tsfms)
