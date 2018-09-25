60f3f6eec7663d044c90b52b4805b7c1025ecbdc
Apollo <sushi90817564@gmail.com>
2018-09-25 21:14:01 +0800

    save

--- salt/resnet.py
+++ salt/resnet.py
@@ -18,8 +18,10 @@ def conv3x3(in_planes, out_planes, stride=1):
 
 
 class BasicBlock(nn.Module):
-    def __init__(self, inplanes, planes, drop, layer_num, block_num, stride=1, downsample=None, writer=None):
-        super().__init__()
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
         self.conv1 = conv3x3(inplanes, planes, stride)
         self.bn1 = nn.BatchNorm2d(planes)
         self.relu = nn.ReLU(inplace=True)
@@ -27,23 +29,22 @@ class BasicBlock(nn.Module):
         self.bn2 = nn.BatchNorm2d(planes)
         self.downsample = downsample
         self.stride = stride
-        # self.drop = nn.Dropout2d(drop)
-        self.writer = writer
-        self.layer_num = layer_num
-        self.block_num = block_num
         self.channel_gate = ChannelGate(planes)
         self.spatial_gate = SpatialGate(planes)
 
-    def forward(self, x, global_step):
+    def forward(self, x):
         residual = x
+
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
-        # out = self.drop(out)
+
         out = self.conv2(out)
         out = self.bn2(out)
+
         if self.downsample is not None:
             residual = self.downsample(x)
+
         out += residual
         out = self.relu(out)
 
@@ -51,45 +52,25 @@ class BasicBlock(nn.Module):
         g2 = self.spatial_gate(out)
         out = (g1 + g2) * out
 
-        if self.writer and self.block_num == 3:
-            log_grad(writer=self.writer, model=self.conv2, tag=f'encoder_layer{self.layer_num}', global_step=global_step)
-
         return out
 
 
-class ConvBlock(nn.Module):
-    def __init__(self, in_c, out_c, kernel_size, stride, padding=0):
-        super().__init__()
-        self.conv = nn.Conv2d(in_c, out_c, kernel_size, stride=stride, padding=padding, bias=False)
-        self.bn = nn.BatchNorm2d(out_c)
-
-    def forward(self, x):
-        x = self.conv(x)
-        x = self.bn(x)
-        x = torch.relu(x)
-        return x
-
-
-class MySequential(nn.Module):
-    def __init__(self, *models):
-        super().__init__()
-        self.models = nn.ModuleList(models)
-
-    def forward(self, x, global_step):
-        for model in self.models:
-            x = model(x, global_step)
-        return x
-
-
 class ResNet(nn.Module):
-    def __init__(self, layers, block, drop, writer=None):
+
+    def __init__(self, block, layers, num_classes=1000):
         self.inplanes = 64
-        super().__init__()
-        self.conv1 = ConvBlock(1, 64, 7, stride=1, padding=3)
-        self.layer1 = self._make_layer(block, 64, layers[0], drop, writer, 1, stride=2)
-        self.layer2 = self._make_layer(block, 128, layers[1], drop, writer, 2, stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], drop, writer, 3, stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], drop, writer, 4, stride=2)
+        super(ResNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AvgPool2d(7, stride=1)
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -98,35 +79,60 @@ class ResNet(nn.Module):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
 
-    def _make_layer(self, block, planes, n_blocks, drop, writer, layer_num, stride=1):
+    def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
-        if stride != 1:
+        if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes,
+                nn.Conv2d(self.inplanes, planes * block.expansion,
                           kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(planes),
+                nn.BatchNorm2d(planes * block.expansion),
             )
-        blocks = []
-        blocks.append(block(self.inplanes, planes, drop, layer_num, 1, stride, downsample, writer=writer))
-        self.inplanes = planes
-        for i in range(1, n_blocks):
-            blocks.append(block(self.inplanes, planes, drop, layer_num, i+1, writer=writer))
-        return MySequential(*blocks)
 
-    def forward(self, x, global_step):
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
         x = self.conv1(x)
-        x = self.layer1(x, global_step)
-        x = self.layer2(x, global_step)
-        x = self.layer3(x, global_step)
-        x = self.layer4(x, global_step)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
         return x
 
-def resnet18(**kwargs):
-    model = ResNet(layers=[2,2,2,2],block=BasicBlock,**kwargs)
-    state_dict = model_zoo.load_url(model_urls['resnet18'])
-    state_dict.pop('conv1.weight')
-    model.load_state_dict(state_dict)
+
+def resnet18(pretrained=False, **kwargs):
+    """Constructs a ResNet-18 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet18']))
     return model
 
-def resnet34(**kwargs):
-    return ResNet(layers=[3,4,6,3], block=BasicBlock, **kwargs)
+
+def resnet34(pretrained=False, **kwargs):
+    """Constructs a ResNet-34 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(BasicBlock, [3, 4, 6, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet34']))
+    return model
