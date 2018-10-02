db04021c967fe66e0d49ed8b1e00b1f6c79bedd9
Apollo <sushi90817564@gmail.com>
2018-10-02 09:08:25 +0800

    save

--- salt/block.py
+++ salt/block.py
@@ -4,13 +4,13 @@ from lightai.imps import *
 class ConvBlock(nn.Module):
     def __init__(self, in_c, out_c, kernel_size, stride=1, padding=0):
         super().__init__()
-        self.conv = nn.Conv2d(in_c, out_c, kernel_size, stride=stride, padding=padding)
+        self.conv = nn.Conv2d(in_c, out_c, kernel_size, stride=stride, padding=padding, bias=False)
         self.bn = nn.BatchNorm2d(out_c)
 
     def forward(self, x):
         x = self.conv(x)
-        x = torch.relu(x)
         x = self.bn(x)
+        x = torch.relu(x)
         return x
 
 
