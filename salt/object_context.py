e79e2336a7cd68cf8a7ad5155545b92d7622a1f1
Apollo <sushi90817564@gmail.com>
2018-10-02 17:36:14 +0800

    save

--- salt/object_context.py
+++ salt/object_context.py
@@ -7,13 +7,18 @@ class Attention(nn.Module):
         super().__init__()
         self.key_c = key_c
         self.value_c = value_c
-        self.key = ConvBlock(in_c, key_c, kernel_size=1, activation='leaky')
-        self.query = ConvBlock(in_c, key_c, kernel_size=1, activation='leaky')
+        self.key = ConvBlock(in_c, key_c, kernel_size=3, activation='leaky', padding=1)
+        self.query = ConvBlock(in_c, key_c, kernel_size=3, activation='leaky', padding=1)
         # self.value = ConvBlock(in_c, value_c, kernel_size=1)
         # self.conv1 = ConvBlock(value_c, in_c, kernel_size=1)
 
     def forward(self, x):
         bs, channel, height, width = x.shape
+        value = x
+
+        p_row = torch.linspace(0, 1, steps=height, device='cuda').expand(height, -1).transpose(0, 1)
+        p_column = torch.linspace(0, 1, steps=width, device='cuda').expand(width, -1)
+        x = x + p_row + p_column
 
         w1 = self.key(x).view(bs, self.key_c, -1)
         w2 = self.query(x).view(bs, self.key_c, -1)
@@ -21,7 +26,7 @@ class Attention(nn.Module):
         w = w / self.key_c**0.5
         w = F.softmax(w, dim=-1)
 
-        value = x.view(bs, channel, -1).permute(0, 2, 1)
+        value = value.view(bs, channel, -1).permute(0, 2, 1)
 
         out = torch.matmul(w, value)
         out = out.permute(0, 2, 1).view(bs, channel, height, width)
