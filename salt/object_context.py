70acde37414f052e98ccc8142e2e46705c35001f
Apollo <sushi90817564@gmail.com>
2018-10-02 16:05:13 +0800

    save

--- salt/object_context.py
+++ salt/object_context.py
@@ -8,14 +8,16 @@ class Attention(nn.Module):
         self.key_c = key_c
         self.value_c = value_c
         self.key = ConvBlock(in_c, key_c, kernel_size=1)
+        self.query = ConvBlock(in_c, key_c, kernel_size=1)
         self.value = ConvBlock(in_c, value_c, kernel_size=1)
         self.conv1 = ConvBlock(value_c, in_c, kernel_size=1)
 
     def forward(self, x):
         bs, channel, height, width = x.shape
 
-        w = self.key(x).view(bs, self.key_c, -1)
-        w = torch.matmul(w.permute(0, 2, 1), w)
+        w1 = self.key(x).view(bs, self.key_c, -1)
+        w2 = self.query(x).view(bs, self.key_c, -1)
+        w = torch.matmul(w1.permute(0, 2, 1), w2)
         w = w / self.key_c**0.5
         w = F.softmax(w, dim=-1)
 
