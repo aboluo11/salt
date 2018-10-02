b57dbd7e5bbc9a603ae3ccb76f8515d4a7163b27
Apollo <sushi90817564@gmail.com>
2018-10-02 14:08:02 +0800

    save

--- salt/unet.py
+++ salt/unet.py
@@ -70,13 +70,9 @@ class UnetBlock(nn.Module):
         self.writer = writer
         self.layer_num = layer_num
         self.tag = f'decode_layer{layer_num}'
-
-        self.ob_context = ObjectContext(feature_c, feature_c//2, feature_c//2, feature_c)
         self.sc = SCBlock(out_c)
 
     def forward(self, feature, x, global_step=None):
-        if self.feature_width != 101:
-            feature = self.ob_context(feature)
         x = self.upconv(x, output_size=feature.shape)
         out = self.conv1(torch.cat([x, feature], dim=1))
         out = self.conv2(out)
