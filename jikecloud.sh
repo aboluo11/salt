eb657d29e2a731869c0811643d8ef679c32a63da
Apollo <sushi90817564@gmail.com>
2018-10-09 21:22:47 +0800

    save

--- jikecloud.sh
+++ jikecloud.sh
@@ -1,9 +1,11 @@
+#!/usr/bin/env bash
 cp /data/inputs.zip inputs.zip
 unzip inputs.zip -d inputs > /dev/null
 rm inputs.zip
 git clone https://github.com/aboluo11/lightai.git lightai_source
 git clone https://github.com/aboluo11/salt.git salt_source
 cp salt_source/main_git.ipynb main.ipynb
+cp salt_source/update.sh update.sh
 ln -s salt_source/salt salt
 ln -s lightai_source/lightai lightai
 pip install --no-cache-dir tensorboardX dill torch_nightly -f https://download.pytorch.org/whl/nightly/cu90/torch_nightly.html
