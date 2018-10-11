9eb3afc2da60091806a794a87b318ec833e44b8d
Apollo <sushi90817564@gmail.com>
2018-10-11 13:34:44 +0800

    save

--- jikecloud.sh
+++ jikecloud.sh
@@ -9,3 +9,4 @@ cp salt_source/update.sh update.sh
 ln -s salt_source/salt salt
 ln -s lightai_source/lightai lightai
 pip install --no-cache-dir tensorboardX dill torch_nightly -f https://download.pytorch.org/whl/nightly/cu90/torch_nightly.html
+mkdir models
