9ed5fc5b4ea203ebbddb054cc177310b2682776f
Apollo <sushi90817564@gmail.com>
2018-10-12 09:20:18 +0800

    save

--- salt/resnet.py
+++ salt/resnet.py
@@ -51,7 +51,7 @@ class BasicBlock(nn.Module):
 
 class ResNet(nn.Module):
 
-    def __init__(self, block, layers, num_classes=1000):
+    def __init__(self, block, layers, dilations, num_classes=1000):
         self.inplanes = 64
         super(ResNet, self).__init__()
         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
@@ -59,10 +59,10 @@ class ResNet(nn.Module):
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0], stride=2, dilation=2)
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, dilation=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2, dilation=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2, dilation=2)
+        self.layer1 = self._make_layer(block, 64, layers[0], stride=2, dilation=dilations[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, dilation=dilations[1])
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2, dilation=dilations[2])
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2, dilation=dilations[3])
         self.avgpool = nn.AvgPool2d(7, stride=1)
         self.fc = nn.Linear(512 * block.expansion, num_classes)
 
